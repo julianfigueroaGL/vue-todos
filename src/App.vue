824bcaa1757bf283bfa2531e1ca0c72ece5d9d07
Julian Figueroa Torres <julian.figueroa@Julian-Figueroa-MacBook-Pro.local>
2020-02-09 00:33:19 -0500

    Add Vue Router and Axios to invoke HTTPS Endpoints

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,16 @@
 <template>
 	<div id="app">
-		<Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+		<Header />
+		<router-view />
 	</div>
 </template>
 
 <script>
-import Todos from './components/Todos';
+import Header from './components/layout/Header';
 export default {
 	name: 'App',
 	components: {
-		Todos
-	},
-	data() {
-		return {
-			todos: [
-				{
-					id: 1,
-					title: 'Todo One',
-					completed: false
-				},
-				{
-					id: 2,
-					title: 'Todo Two',
-					completed: true
-				},
-				{
-					id: 3,
-					title: 'Todo Three',
-					completed: false
-				}
-			]
-		};
-	},
-	methods: {
-		deleteTodo(id) {
-			this.todos = this.todos.filter(todo => todo.id !== id);
-		}
+		Header
 	}
 };
 </script>
@@ -50,5 +25,18 @@ export default {
 body {
 	font-family: Arial, Helvetica, sans-serif;
 	line-height: 1.4;
+}
+
+.btn {
+	display: inline-block;
+	border: none;
+	background: #555;
+	color: #fff;
+	padding: 7px 20px;
+	cursor: pointer;
+}
+
+.btn:hover {
+	background: #666;
 }
 </style>
